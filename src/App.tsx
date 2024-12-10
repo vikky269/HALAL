3c80666f91d7e300d59c8cd57b867dc1e7e2232b
Amune victor <viktorefi2001@gmail.com>
2024-12-10 21:17:20 +0100

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import Navbar from "./components/Navbar/Navbar"
 import Hero from "./components/Hero/Hero"
-import EthicalBanking from "./components/EthicalBanking/EthicalBanking"
-import Community from "./components/Community/Community"
-import Stats from "./components/Stats/Stats"
+// import EthicalBanking from "./components/EthicalBanking/EthicalBanking"
+// import Community from "./components/Community/Community"
+// import Stats from "./components/Stats/Stats"
 
 
 function App() {
