c97c03fbb685de57dbfb4f50c16a84886c767c77
Amune victor <viktorefi2001@gmail.com>
2024-12-12 17:44:02 +0100

    whala

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -136,7 +136,7 @@ const Hero: React.FC = () => {
 
         <div className="flex flex-col justify-center items-center mx-auto px-6 py-3">
 
-          <div className="text-center flex flex-col items-center mb-8 px-6 py-6 border-2 border-red-950">
+          <div className="text-center flex flex-col items-center mb-8 px-6 py-6">
             <h1 className="text-3xl font-bold whitespace-nowrap text-[#0066f4] text-center w-full rounded-sm py-3 px-12 mb-6 bg-gradient-to-b from-[#ffffff] to-[#dfe8f6]">
               A BANK FOR
             </h1>
